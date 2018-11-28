e4f31f097110dffb3347e095a469ce59cbde2860
Fabio SP <sarmentopedrofabio@gmail.com>
2018-11-28 23:37:37 +0100

    Add Product class + Context + Seed

---
+++ ProductManagerApi/ProductManagerApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductManagerApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public bool Active { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ProductManagerApi/ProductManagerApi/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductManagerApi.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProductContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ProductContext>>()))
+            {
+                if (context.Products.Any())
+                {
+                    return;
+                }
+
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "VISI/pocket",
+                        Category = "firstClass",
+                        Active = true,
+                        Price = 999
+                    },
+
+                    new Product
+                    {
+                        Name = "VISI/frame",
+                        Category = "secondClass",
+                        Active = true,
+                        Price = 888.0
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
